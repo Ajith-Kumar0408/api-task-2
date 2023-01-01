0da055a7c7f755aeba5b2f0765af5e5ae7e91635
Ajith-Kumar0408 <ajithak0093@gmail.com>
2023-01-01 19:15:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*var cont = document.createElement("div");
+cont.setAttribute("class","center-part");
+
+var books = document.createElement("div");
+books.setAttribute("class","main");
+
+var heading=document.createElement("h1");
+heading.setAttribute("class","heading");
+heading.innerHTML=<i class="fa-solid fa-book">Books API</i>;
+
+
+button.onclick = function fun(e)
+{
+    e.preventDefault();
+    window.open('https://bible-api.com/john 3:16');
+}*/
