5d30d469994520e79e7fd471f0f3f86ebeb085ab
Ajith-Kumar0408 <ajithak0093@gmail.com>
2023-01-07 13:43:09 +0530

    end

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-/*var cont = document.createElement("div");
-cont.setAttribute("class","center-part");
 
-var books = document.createElement("div");
-books.setAttribute("class","main");
-
-var heading=document.createElement("h1");
-heading.setAttribute("class","heading");
-heading.innerHTML=<i class="fa-solid fa-book">Books API</i>;
+var container=document.createElement("div");
+container.setAttribute("class","container");
+var row=document.createElement("div");
+row.setAttribute("class","row");
+container.append(row);
 
 
-button.onclick = function fun(e)
-{
-    e.preventDefault();
-    window.open('https://bible-api.com/john 3:16');
-}*/
+var res=fetch("https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=223");
+res.then((data)=>data.json()).then((data1)=>foo(data1));
+    function foo(data1){
+          for(i=0;i<data1.length;i++){
+              row.innerHTML+= `<div class="col-md-4">
+                               <div class="card mb-3" style="max-width: 18rem;">
+                                <img src=${data1[i].image} class="card-img-top">
+                                 <div class="card-body text-danger">
+                                  <h2 class="card-title"><strong>Name : ${data1[i].name}</strong></h2>
+                               <p class="card-text">Gender:${data1[i].gender}</p>
+                               <p class="card-text">First Episode : ${data1[i].firstEpisode}</p>
+                               <p class="p-text">Voiced By: ${data1[i].voicedBy}</p>
+                               <a href="${data1[i].url}" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                            </div>
+                        </div>`;
+      document.body.append(container);
+          }
+}
